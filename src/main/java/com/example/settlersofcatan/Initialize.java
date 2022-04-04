7572a4d0a790807a87815f0f3b209a936cd2ab37
slhscs299 <gordonj2016@outlook.com>
2022-04-04 12:05:42 -0500

    init

---
+++ src/main/java/com/example/settlersofcatan/Initialize.java
@@ -0,0 +1,15 @@
+package com.example.settlersofcatan;
+
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Initialize {
+    public static Map<String, Image> tiles = new HashMap<>();
+    public static Map<String, Image> resources = new HashMap<>();
+    public static Map<String, Image> buildings = new HashMap<>();
+    public static Map<String, Image> numberTokens = new HashMap<>();
+    public static Map<String, Image> robber = new HashMap<>();
+
+}
