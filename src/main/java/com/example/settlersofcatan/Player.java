fe873075c7f1bbb9c6513507e1e32c6860b9f63a
slhscs316 <zijunyu2018@gmail.com>
2022-04-06 12:18:28 -0500

    create class, initiate in player class

--- src/main/java/com/example/settlersofcatan/Player.java
+++ src/main/java/com/example/settlersofcatan/Player.java
@@ -1,8 +1,15 @@
 package com.example.settlersofcatan;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Player {
+
+    Stack<ResourceCard> resourceDeck = new Stack<ResourceCard>();
+    Stack<DevCard> devDeck = new Stack<DevCard>();
+    ArrayList<Settlement> settlements = new ArrayList<Settlement>();
+    ArrayList<City> cities = new ArrayList<City>();
+    int cardCount, victoryPoints;
 
     public int getCardCount(){
         return 0;
@@ -13,5 +20,21 @@ public class Player {
     public ArrayList<Edge> getBuildableEdges(){
         return new ArrayList<>();
     }
+    public void takeAllResources(Stack ResourceCard) {
+
+    }
+    public void stealCard(boolean Player) {
+
+    }
+    public boolean
+    
+
+
+
+
+
+
+
+
 
 }
