2b4455dfc19542b3ba4cf19362852a04add02345
tetuji486597 <gordonj2016@outlook.com>
2022-04-08 22:50:26 -0500

    added controller to gameboard

---
+++ src/main/java/com/example/settlersofcatan/GameBoardController.java
@@ -0,0 +1,14 @@
+package com.example.settlersofcatan;
+
+import javafx.fxml.FXML;
+
+import java.io.FileNotFoundException;
+
+public class GameBoardController {
+
+
+    @FXML
+    public void initialize() throws FileNotFoundException {
+
+    }
+}
